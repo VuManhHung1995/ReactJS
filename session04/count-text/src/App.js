296359fdcbab196c4b800683789cd771e0b87b5c
HungVM <manhhung11a@gmail.com>
2023-04-23 09:55:50 +0900

    a

--- session04/count-text/src/App.js
+++ session04/count-text/src/App.js
@@ -1,12 +1,28 @@
 import "./App.css";
 import CountClick from "./components/CountClick";
 import CountText from "./components/CountText";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
+import OrderForm from "./components/OrderForm";
+import ProductList from "./components/ProductList";
+import SearchAndFilter from "./components/SearchAndFilter";
+import ShippingCalculator from "./components/ShippingCalculator";
+import TaskList from "./components/TaskList";
+import TemperatureConverter from "./components/TemperatureConverter";
 
 function App() {
   return (
     <div className="App">
       <CountText />
       <CountClick />
+      <Counter />
+      <OrderForm />
+      <ProductList />
+      <TaskList />
+      <SearchAndFilter />
+      <Form />
+      <TemperatureConverter />
+      <ShippingCalculator />
     </div>
   );
 }
